2986dfc8d4f8cfa8ae22692f874e915a7f5ef3f0
rownaknira <rownaknira@yahoo.com>
2015-11-04 10:33:28 +0000

    Added static page home

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    def	full_title(page_title	=	'')		                                        #	Method	def,	optional	arg
+				base_title	=	"Framgia e-learning System"		                    #	Variable	assignment
+				if	page_title.empty?												#	Boolean	test
+						base_title													#	Implicit	return
+				else
+						"#{page_title}	|	#{base_title}"							#	String	interpolation
+				end
+    end
+
 end
