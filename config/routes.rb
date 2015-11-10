8e59bc2cfe5fb6673b7f8c7e9ec837bc4ec140cd
Rownaknira <rownaknira@yahoo.com>
2015-11-10 11:05:54 +0600

    Adding category controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get 'categories/new'
+
   root 'static_pages#home'
-  get	 'login'  =>  'sessions#new'
+  get 'login'  =>  'sessions#new'
   post  'login'  =>  'sessions#create'
-  delete	'logout'  =>  'sessions#destroy'
-  get	'signup'  =>  'users#new'
-  resources	:users
+  delete 'logout'  =>  'sessions#destroy'
+  get 'signup'  =>  'users#new'
+  resources :users
+
+  resources :categories,only: [:index]
+  namespace :admin do
+    resources :categories,only: [:new, :create, :edit, :update, :destroy]
+  end
 end
